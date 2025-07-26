3011209749e8ab71782c5dd180ab1ed71ee52e7f
ajeeshRS <ajeeshrs79@gmail.com>
2025-07-26 05:53:51 +0530

    update lib.rs

--- programs/anchor-amm/src/lib.rs
+++ programs/anchor-amm/src/lib.rs
@@ -14,7 +14,6 @@ declare_id!("FgXKw2uG31zSn1qvMwLaRAm9BPWHLDBS1wzUzuBM5JhE");
 #[program]
 pub mod anchor_amm {
 
-
     use super::*;
 
     pub fn initialize(
@@ -26,11 +25,27 @@ pub mod anchor_amm {
         ctx.accounts.initialize(seed, fee, authority, ctx.bumps)?;
         Ok(())
     }
-    pub fn deposit(
-        ctx: Context<Deposit>,
-    ) -> Result<()> {
+    pub fn deposit(ctx: Context<Deposit>, amount: u64, max_x: u64, max_y: u64) -> Result<()> {
+        ctx.accounts.deposit(amount, max_x, max_y)?;
         Ok(())
     }
 
+    pub fn swap(ctx: Context<Swap>, amount: u64, is_x: bool, min: u64) -> Result<()> {
+        ctx.accounts.swap(amount, min, is_x)?;
+        Ok(())
+    }
+
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64, min_x: u64, min_y: u64) -> Result<()> {
+        ctx.accounts.withdraw(amount, min_x, min_y)?;
+        Ok(())
+    }
+    pub fn unlock(ctx: Context<Update>) -> Result<()> {
+        ctx.accounts.unlock()?;
+        Ok(())
+    }
+    pub fn lock(ctx: Context<Update>) -> Result<()> {
+        ctx.accounts.lock()?;
+        Ok(())
+    }
 
 }
